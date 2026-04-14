5dabf73d3dd390db5ad87de610e4da49a4e6da15
dialryckx <ismaelbrus68@gmail.com>
2026-04-14 16:46:40 +0300

    Fixed bugs

--- httpserver.py
+++ httpserver.py
@@ -2,13 +2,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Sending HTTP status 200 if everything is ok
         self.send_response(200)
-        # Type of content
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
-        
-        # Text that will be on server
         response_text = "<h1>Hello! This is your Python server.</h1>"
         self.wfile.write(response_text.encode("utf-8"))
 
